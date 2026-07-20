ca9e45c22abea654112c7c4b85caf39601d548ed
ALY ISMAILOV <88724200+neuropromotion@users.noreply.github.com>
2026-07-20 14:39:51 +0300

    Update constants.py

--- final_train_test_inference/inference/constants.py
+++ final_train_test_inference/inference/constants.py
@@ -2,25 +2,20 @@ from __future__ import annotations
 
 from pathlib import Path
 
+# local dirs
 TOTAL_INFERENCE = Path(__file__).resolve().parent
 FINAL_VERSION = TOTAL_INFERENCE.parent
 WORKSPACE = FINAL_VERSION.parent
 
 # paths on my workspace
-CONFIG_PATH = FINAL_VERSION / "FINAL_CONFIG_AND_FIGURES" / "target_config.json"
-MODELS_ROOT = FINAL_VERSION / "final_train" / "results"
-ENSEMBLE_ID = "catboost_tabm_resnet_stack"
+MODELS_ROOT = FINAL_VERSION / "final_train" / "results" # model weights (available on kaggle)
 WEIGHTS_DIR = MODELS_ROOT / "ensemble" / ENSEMBLE_ID / "weights"
-K1_REF_PATH = FINAL_VERSION / "final_train" / "splits" / "sc_k1" / "X_train.parquet"
-GENE_LENGTHS_PATH = TOTAL_INFERENCE / "df_gene_mapping.parquet"
-MRNA_NAMES_PATH = TOTAL_INFERENCE / "mRNA_names.json"
-GENE_MAPPING_PATH = TOTAL_INFERENCE / "ensembl_gene_mapping.csv"
-GENE_MAPPING_CANDIDATES = (
-    GENE_MAPPING_PATH,
-    WORKSPACE / "inference" / "ensembl_gene_mapping.csv",
-    Path("/mnt/jack-5/amismailov/ensembl_gene_mapping.csv"),
-)
-
+K1_REF_PATH = FINAL_VERSION / "final_train" / "splits" / "sc_k1" / "X_train.parquet" # Imputing ref - singlecell train dataset on K1 level (available on kaggle)
+ENSEMBLE_ID = "catboost_tabm_resnet_stack"
+CONFIG_PATH = "target_config.json"
+GENE_LENGTHS_PATH = "df_gene_mapping.parquet"
+MRNA_NAMES_PATH = "mRNA_names.json"
+GENE_MAPPING_PATH = "ensembl_gene_mapping.csv"
 STACK_MODELS = ("catboost_optuna", "tabm", "resnet")
 
 
